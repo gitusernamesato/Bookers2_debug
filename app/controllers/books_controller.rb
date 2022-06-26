0271e3004a7b0a4f39d403daafb0f1fdf90a7659
EC2 Default User <shinya.sin09@gmail.com>
2022-06-26 05:12:32 +0000

    [finsh]bookers2_debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @book = Book.find(params[:id])
     @user = @book.user
@@ -45,5 +47,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 end
