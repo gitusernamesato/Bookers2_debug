4d6217222ec65eff274363ae3c43b4b16be83a36
EC2 Default User <shinya.sin09@gmail.com>
2022-06-28 16:45:07 +0000

    [add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- 
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :name, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
   # def get_profile_image
   #   (profile_image.attached?) ? profile_image : 'no_image.jpg'
   # end
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -23,4 +28,17 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height])
   end
+
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
+
 end
