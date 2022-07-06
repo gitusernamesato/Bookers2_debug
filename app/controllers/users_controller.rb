40e72798d3f757f2f1b95928d9f809d1acea4044
EC2 Default User <shinya.sin09@gmail.com>
2022-07-06 03:38:41 +0000

    [update]favo_ranking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    # @books = @user.books
+    @books = @user.books.includes(:favorites).sort {|a,b| b.favorites.size <=> a.favorites.size}
     @book = Book.new
   end
 
@@ -16,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update 
+  def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
